78bc4c57ead9ad66cbfc1b95449b82c6d0c8a3b4
林姿慧 <n0923609898@yahoo.com.tw>
2023-12-03 20:03:12 +0800

    zzkk
    2-1 3-1

--- frontend/frontend/src/views/Karen.vue
+++ frontend/frontend/src/views/Karen.vue
@@ -1,3 +1,272 @@
 <template>
-  <div class="karen"></div>
+  <div>
+    <div class="button-container">
+        <button type="button" onclick="handleButtonClick('button1')">
+          <el-icon style="color: red;"><ArrowUp /></el-icon></button>
+          <button type="button" onclick="handleButtonClick('button2')"><el-icon><Edit /></el-icon>會議基本資訊</button>
+        <button type="button" onclick="handleButtonClick('button3')">
+          <el-icon>
+            <Link />
+        </el-icon>
+      </button>
+       
+    </div>
+    <div class="form-container">
+      <table class="form-table">
+        <tr>
+          <td class="left-bar">
+            <label for="meetingName">會議名稱</label>
+          </td>
+          <td class="input-cell">
+            <input type="text" id="meetingName" v-model="meetingName" class="text-input" :placeholder="placeholder" @focus="clearPlaceholder" @blur="restorePlaceholder">
+          </td>
+        </tr>
+        <tr>
+          <td class="left-bar">
+            <label for="time">時間</label>
+          </td>
+          <td class="input-cell">
+            <div class="demo-range">
+              <el-time-picker
+                v-model="value1"
+                is-range
+                range-separator="To"
+                start-placeholder="Start time"
+                end-placeholder="End time"
+                placeholder="'-'"
+              />
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td class="left-bar">
+            <label for="attend">出席人員</label>
+          </td>
+          <td class="input-cell">
+            <input type="text" id="attend" v-model="attend" class="text-input" placeholder="-">
+          </td>
+        </tr>
+        <tr>
+          <td class="left-bar">
+            <label for="place">地點</label>
+          </td>
+          <td class="input-cell">
+            <input type="place" id="place" v-model="place" class="text-input" placeholder="-">
+          </td>
+        </tr>
+        <tr>
+          <td class="left-bar">
+            <label for="absent">缺席人員</label>
+          </td>
+          <td class="input-cell">
+            <input type="absent" id="absent" v-model="absent" class="text-input" placeholder="-">
+          </td>
+        </tr>
+        <tr>
+          <td class="left-bar">
+            <label for="record">紀錄人員</label>
+          </td>
+          <td class="input-cell">
+            <input type="record" id="record" v-model="record" class="text-input" placeholder="-">
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="cart_container">
+      <button class="add_cartButton" type="button" @click="add_cart"><el-icon><Plus /></el-icon></button>
+      <div class="additional-textarea">
+        <div class="textarea-container">
+          <textarea id="textArea" v-model="textArea" placeholder="請輸入內容" style="height: 70px;"></textarea>
+          <button class="edit_textButton" type="button" @click="edit_textArea"><el-icon><MoreFilled /></el-icon></button>
+        </div>
+        <div class="split-line" style="width: 100%;"></div>
+        <textarea id="tag" v-model="tag" placeholder="選擇標籤類型並建立標籤" style="height: 20px;"></textarea>
+      </div>
+    </div>
+    <el-alert class = "popUp_msg" title="會議紀錄連結已複製" type="success" show-icon />
+    <el-alert class = "popUp_msg" title="已刪除會議紀錄" type="warning" show-icon :style="{ backgroundColor: '#FFEFF0',color: '#EB3B23' }" />
+    <el-alert class = "popUp_msg" title="已儲存會議基本資訊" type="success" show-icon />
+    <el-alert class = "popUp_msg" title="您已永久刪除會議紀錄" type="info" show-icon />
+    
+    <el-alert class="popUp_recover"
+      title="成功復原會議紀錄"
+      type="success"
+      show-icon
+      :style="{ backgroundColor: '#FFFFFF'}"
+    >
+    <!-- 不知道怎麼單獨改title的顏色，因為title的屬性好像是跟著圖標的 -->
+    <a href="link-url" :style="{ color: '#67C23A' }">點擊檢視復原檔案</a>
+</el-alert>
+
+    </div>
 </template>
+
+<script >
+
+import axios from 'axios';
+import { ref } from 'vue';
+
+export default {
+  name:'meeting',
+
+  data() {
+    return {
+      meetingName: "",
+      placeholder: "輸入會議名稱",
+    };
+  },
+  methods: {
+    clearPlaceholder() {
+      this.placeholder = "";
+    },
+    restorePlaceholder() {
+      if (!this.meetingName) {
+        this.placeholder = "輸入會議名稱";
+      }
+    },
+  },
+};
+
+const value1 = ref<[Date, Date]>([
+  new Date(2016, 9, 10, 8, 40),
+  new Date(2016, 9, 10, 9, 40),
+])
+</script>
+<style>
+.demo-range .el-date-editor {
+  margin: 8px;
+}
+
+.form-table {
+  border-radius: 4px;
+  margin-top: 20px;
+  margin-left: 448px;
+  width: 910px;
+  border-collapse: collapse;
+}
+
+.left-bar {
+  padding: 12px;
+  padding-left: 16px;
+  width: 73px;
+  text-align: left;
+  font-size: 15px;
+  
+  border: 1px solid #ccc;
+  background-color: #EBEEF5; /* 添加背景顏色 */
+}
+
+.input-cell {
+  padding: 8px;
+  border: 1px solid #ccc;
+  width: 837px;
+}
+
+.text-input {
+  width: 100%;
+  border: none;
+  outline: none;
+  /* -webkit-appearance: none; */
+  font-size: 14px;
+}
+
+.additional-textarea {
+  border-radius: 4px;
+  width: 890px;
+  margin-left:15px;
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ccc; /* 大框框的边框样式，你可以根据需要调整颜色和样式 */
+ 
+
+}
+.textarea-container {
+  display: flex;
+  align-items: flex-start;
+}
+
+textarea {
+  font-size: 15px;
+  width: 93%;
+  border: 0px; /* 移除 textarea 的边框 */
+  resize: none; /* 防止調整 textarea 的大小 */
+  outline: none; /* 移除点击时的边框 */
+}
+
+.split-line {
+  border-top: 1px solid #ccc; 
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.button-container {
+    display: flex;
+    height:40px;
+    width:910px;
+    margin-top: 68px;
+    margin-left: 448px;
+    margin-bottom: 0px;
+}
+.button-container button:first-child {
+    margin-right: auto; /* 将第一个按钮推到最左边 */
+    width: 50px;
+    background-color: white;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+.button-container button:nth-child(2) {
+  margin-right: 20px;
+  width: 140px;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  
+}
+.button-container button:nth-child(2) .el-icon {
+  margin: 10px; /* 设置图标与文本之间的右边距 */
+}
+.button-container button:nth-child(3) {
+  margin-right: 0px; 
+  width: 50px;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+.cart_container {
+  margin-left: 383px;
+  display: flex;
+  align-items: flex-start;
+}
+.add_cartButton{
+  height: 40px;
+  width: 50px;
+  margin-top: 15px;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+.edit_textButton{
+  height: 35px;
+  width: 50px;
+  margin-left: 0px;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+.popUp_msg{
+  width: 500px;
+  height: 50px;
+  margin: 20px 0 0;
+  font-size: 0px;
+}
+.popUp_recover{
+  width: 700px;
+  height: 80px;
+  margin: 20px 0 0;
+  box-shadow: 0 5px 8px rgba(0, 0, 0, 0.4); /* 阴影样式 */
+  
+}
+
+</style>
